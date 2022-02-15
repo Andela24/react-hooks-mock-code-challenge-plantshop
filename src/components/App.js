0c96544efd1d46dbbd9ecca7deb32a1680d29026
Andela Stevanovic <andjela.s166@gmail.com>
2022-02-15 09:09:45 -0500

    commit tree bb9aaa73375fd5dd906088808ac0d8c4d93bbb2a

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Header from "./Header";
 import PlantPage from "./PlantPage";
 
 function App() {
+  const [listAllPlants, setListAllPlants] = useState([])
+  const [searchInput, setSearchInput] = useState("")
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+    .then(res => res.json())
+    .then(listAllPlants => setListAllPlants(listAllPlants))
+  }, [])
+
+  
+  const filterPlants = listAllPlants.filter(plant => plant?.name?.toLowerCase().includes(searchInput.toLowerCase()))
+
   return (
     <div className="app">
       <Header />
-      <PlantPage />
+      <PlantPage listAllPlants= {filterPlants} setListAllPlants={setListAllPlants} setSearchInput={setSearchInput}/>
     </div>
   );
 }
